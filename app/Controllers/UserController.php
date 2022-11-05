fb28d99a81ba909316ab2371e5b367db617a1e21
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2022-11-05 13:47:58 -0300

    Configurais iniciais, criação de UserModel e UserController

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\UserModel;
+
+class UserController extends BaseController
+{
+    private $userModel;
+
+    public function __construct()
+    {
+        $this -> userModel = new UserModel();
+    }
+    public function index()
+    {
+        return view('users', ['users' =>  $this -> userModel -> findAll()] );
+    }
+}
